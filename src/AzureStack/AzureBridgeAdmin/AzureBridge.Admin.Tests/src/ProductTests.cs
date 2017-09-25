7f66fd40cda4041d4096f29cd4897df011a8dd40
Jeffrey A. Robinson <jeffrey.robinson@microsoft.com>
2017-09-25 10:25:58 -0700

    AzureBridge

---
+++ src/AzureStack/AzureBridgeAdmin/AzureBridge.Admin.Tests/src/ProductTests.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+//
+
+using Microsoft.AzureStack.Management.AzureBridge.Admin;
+// using Microsoft.AzureStack.Management.AzureBridge.Admin.Models;
+using System;
+using Xunit;
+
+namespace AzureBridge.Tests
+{
+    public class ProductTests : AzureBridgeTestBase
+    {
+
+        
+        [Fact]
+        public void TestListPublishedProducts() {
+            RunTest((client) => {
+                var list = client.PublishedProducts.List();
+                Common.WriteIPagesToFile(list, client.PublishedProducts.ListNext, "TestListPublishedProducts");
+            });
+        }
+
+    }
+}
