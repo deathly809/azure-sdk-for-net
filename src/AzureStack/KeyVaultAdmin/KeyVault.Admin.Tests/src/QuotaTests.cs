20f83cc4838701722dc57bcd71ad1ebaa1a84ada
Jeffrey A. Robinson <jeffrey.robinson@microsoft.com>
2017-11-06 13:40:18 -0800

    KeyVault with Tests

---
+++ src/AzureStack/KeyVaultAdmin/KeyVault.Admin.Tests/src/QuotaTests.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+//
+
+using Microsoft.AzureStack.Management.KeyVault.Admin;
+using Microsoft.AzureStack.Management.KeyVault.Admin.Models;
+using System;
+using Xunit;
+
+namespace KeyVault.Tests
+{
+    public class SubscriberUsageAggregateTests : KeyVaultTestBase
+    {
+
+        private void ValidateQuota(Quota ua) {
+            Assert.NotNull(ua);
+            Assert.NotNull(ua.Id);
+            Assert.NotNull(ua.Name);
+            Assert.NotNull(ua.Type);
+        }
+
+        [Fact]
+        public void TestListSubscriberUsageAggregatesFromLastTwoDays() {
+            RunTest((client) => {
+
+                var quotas = client.Quotas.List("local");
+                quotas.ForEach(ValidateQuota);
+            });
+        }
+
+    }
+}
