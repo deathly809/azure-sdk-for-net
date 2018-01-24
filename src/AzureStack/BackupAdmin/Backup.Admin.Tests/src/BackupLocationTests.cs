2e6e8595a0bcc9ea60dcbe0bec31e932a14b3b50
Jeffrey A. Robinson <jeffrey.robinson@microsoft.com>
2018-01-24 14:48:42 -0800

    Backup Generation

---
+++ src/AzureStack/BackupAdmin/Backup.Admin.Tests/src/BackupLocationTests.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+//
+
+using Microsoft.AzureStack.Management.Backup.Admin;
+using Microsoft.AzureStack.Management.Backup.Admin.Models;
+using System;
+using Xunit;
+
+namespace Backup.Tests
+{
+    public class BackupLocationTests : BackupTestBase
+    {
+
+
+        [Fact]
+        public void TestListSubscriberUsageAggregatesFromLastTwoDays() {
+            RunTest((client) => {
+                client.BackupLocations.List("System.local");
+            });
+        }
+
+    }
+}
---
+++ src/AzureStack/BackupAdmin/Backup.Admin.Tests/src/Helpers/BackupTestBase.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+//
+
+using Microsoft.AzureStack.Management.Backup.Admin;
+using Xunit;
+
+namespace Backup.Tests
+{
+
+    public class BackupTestBase : AzureStackTestBase<BackupAdminClient>
+    {
+        public BackupTestBase()
+        {
+            // Empty
+        }
+
+        protected override void ValidateClient(BackupAdminClient client)
+        {
+            // validate creation
+            Assert.NotNull(client);
+
+            // validate objects
+            Assert.NotNull(client.Backups);
+            Assert.NotNull(client.SubscriptionId);
+        }
+    }
+}
