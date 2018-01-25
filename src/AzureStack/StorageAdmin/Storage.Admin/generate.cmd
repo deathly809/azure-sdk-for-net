3540ef3f3f7b2c4d9e6afd2a09fa27a33d0b0add
Jeffrey A. Robinson <jeffrey.robinson@microsoft.com>
2018-01-24 19:50:29 -0800

    Regenerated Storage SDK

--- src/AzureStack/StorageAdmin/Storage.Admin/generate.cmd
+++ src/AzureStack/StorageAdmin/Storage.Admin/generate.cmd
@@ -5,3 +5,6 @@
 
 @echo off
 call %~dp0..\..\..\..\tools\generate.cmd azsadmin/resource-manager/storage latest deathly809 azs.storage.admin azure-rest-api-specs %CD%
+rd Generated /S /Q
+move Storage\Storage.Admin\Generated .
+rd Storage /S /Q
